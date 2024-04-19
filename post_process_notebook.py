1b773b1e6bb18962342094da5c35be179f5b179c
Noah Luna <15202580+ngrayluna@users.noreply.github.com>
2024-04-19 14:01:26 -0700

    Added title mapping

--- post_process_notebook.py
+++ post_process_notebook.py
@@ -1,13 +1,52 @@
 #!/bin/usr/python
 
-# Things to do:
-# Make mapping of .ipynb to title + append it to file
-
-
+import os
 import re
 import argparse
 
+# broken_tmp ={
+#     "Credit_Scorecards_with_XGBoost_and_W&B" : "xgboost",
+#     "Simple_LightGBM_Integration": "lightgbm",
+# }
+
+# no_longer = {
+#         "RayTune_with_wandb": "",
+#         "Weights_&_Biases_with_fastai": "",
+#         "WandB_Prompts_Quickstart":"",    
+# }
+
+title_mapping = {
+    "Intro_to_Weights_&_Biases": "experiments",
+    "Pipeline_Versioning_with_W&B_Artifacts": "artifacts",
+    "Model_Registry_E2E": "models",
+    "W&B_Tables_Quickstart": "tables",
+    "Organizing_Hyperparameter_Sweeps_in_PyTorch_with_W&B": "sweeps",
+    "Using_W&B_Sweeps_with_XGBoost": "xgboost_sweeps",
+    "Simple_PyTorch_Integration": "pytorch",
+    "Huggingface_wandb": "huggingface",
+    "Hyperparameter_Optimization_in_TensorFlow_using_W&B_Sweeps": "tensorflow_sweeps",
+    "Image_Classification_using_PyTorch_Lightning": "lightning",
+    "Simple_TensorFlow_Integration": "tensorflow",
+    "Use_WandbMetricLogger_in_your_Keras_workflow": "keras",
+    "Use_WandbEvalCallback_in_your_Keras_workflow": "keras_table",
+    "Use_WandbModelCheckpoint_in_your_Keras_workflow": "keras_models",
+}
+
+def rename_markdown_file(filename, title_names):
+    "Rename notebook file."
+    # Check if .ipynb name exists in our mapping
+
+    base_name = os.path.basename(filename).split('.')[0]
+    if base_name in title_names:
+        new_filename = title_names[base_name]
+
+        # Rename file
+        print(f"Renaming notebook from {filename} to {new_filename}.md")
+        os.rename(filename, new_filename+".md")
+
+
 def add_import_statement():
+    # Add CTA import statement
     return "import { CTAButtons } from '@site/src/components/CTAButtons/CTAButtons.tsx'\n\n"
 
 def extract_href_links_from_markdown(markdown_text):
@@ -50,6 +89,7 @@ def main(args):
     # Extract href links from the Markdown content
     href_links = extract_href_links_from_markdown(markdown_text)
 
+    # Create CTA button format
     colab_button_markdown = format_CTA_button(href_links)
 
     # Modify the Markdown content (e.g., remove <img> tags and specified comment)
@@ -61,6 +101,10 @@ def main(args):
         file.write(colab_button_markdown)
         #file.write(add_title(title))  # To do
         file.write(cleaned_markdown)
+
+    print("Checking if we need to rename notebook")
+    # Rename notebook
+    rename_markdown_file(args.file, title_mapping)
 
     return
 
